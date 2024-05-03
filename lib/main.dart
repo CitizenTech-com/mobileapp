922d4d8d49321e4259a13be913ff0de13054ed42
Amit Dubey <iamsaagar@yahoo.com>
2024-05-03 10:34:26 +0100

    Initial Draft
    Initial Draft

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CitizenTech());
+}
+
+class CitizenTech extends StatelessWidget {
+  const CitizenTech({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
