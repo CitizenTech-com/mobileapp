0de0da9ee9b48304ca68c4f3a667abd2c8508781
Amit Dubey <iamsaagar@yahoo.com>
2024-05-29 16:35:43 +0100

    Updated module structure

---
+++ lib/modules/authentication_module/signin/controller/login.controller.dart
@@ -0,0 +1,10 @@
+import 'package:Citizen.Tech/modules/authentication_module/repo/login.repo.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final LoginRepository repo;
+  final RxBool isChecked = true.obs;
+  final RxString userName = "".obs;
+  final RxString password = "".obs;
+  LoginController({required this.repo});
+}
